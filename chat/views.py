d876f20a5f0fa7b7424b4e783fe2e64970366f9c
Vladii <vladii@Vladiis-MacBook-Air.local>
2020-12-22 18:27:53 +0300

    fixes1.1

--- chat/views.py
+++ chat/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'chat/index.html', {})
+    return render(request, 'index.html', {})
 
 
 def room(request, room_name):
-    return render(request, 'chat/room.html', {
+    return render(request, 'room.html', {
         'room_name': room_name
     })
