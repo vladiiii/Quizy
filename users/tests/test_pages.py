272a54022a0976bd489e925aafd11679d056b1e4
Eskelai <you@example.com>
2020-12-29 19:01:34 -0800

    Working tests

--- users/tests/test_pages.py
+++ users/tests/test_pages.py
@@ -4,19 +4,21 @@ import pytest
 
 
 @pytest.mark.parametrize('param', [
-    ('login'),
-    ('logout'),
-    ('register')
+    'login',
+    'logout',
+    'register'
 ])
-
 def test_rendering_pages(client, param):
     temp_url = urls.reverse(param)
     resp = client.get(temp_url)
-    assert resp.status_code == 200 # Success
+    assert resp.status_code == 200  # Success
+
 
 # @pytest.mark.django_db
 # def test_user_signup(client, user_data):
 #     user_model = get_user_model()
 #     assert user_model.objects.count() == 0
-#     #signup_url
+#     signup_url = urls.reverse('login')
+#     resp = client.post(signup_url, user_data)
+#     assert user_model.objects.count() == 1
 
